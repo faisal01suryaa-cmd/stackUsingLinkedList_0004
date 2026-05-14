fd1942e83e4bb50a5cc6b08bc494234a3230dc97
faisal01suryaa <faisal01suryaa@gmail.com>
2026-05-14 15:24:12 +0700

    case 2 untuk pemilihan fungsi pop

--- stackUsingLinkedList.cpp
+++ stackUsingLinkedList.cpp
@@ -97,6 +97,18 @@ int main()
             stack.push(value);
             break;
 
+            case 2:
+            if (!stack.isEmpty())
+            {
+                stack.pop();
+            }
+            else
+            {
+                cout << "Stack is empty. Cannot pop." << endl;
+            }
+            break;
+
+
         }
 
     }
