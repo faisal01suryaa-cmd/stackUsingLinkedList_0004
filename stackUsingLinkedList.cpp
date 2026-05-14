7ca8a5f75949d6d090a87539ea10a16e1dd5ffe0
faisal01suryaa <faisal01suryaa@gmail.com>
2026-05-14 15:05:48 +0700

    menambahkan metode push untuk menambahkan elemen ke stack

--- stackUsingLinkedList.cpp
+++ stackUsingLinkedList.cpp
@@ -25,5 +25,15 @@ public:
     {
         top = NULL;
     }
+    int push(int value)
+    {
+        Node *newNode = new Node();
+        newNode->data = value;
+        newNode->next = top;
+        top = newNode;
+        cout << "Push value: " << value << endl;
+        return value;
+    }
+
 
 };
